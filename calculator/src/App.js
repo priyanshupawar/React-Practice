c41e738ea0b953d6c00e415616eec0133bb5c302
priyanshupawar <talk20priyanshu@gmail.com>
2023-01-08 03:30:39 +0530

    react-cal

---
+++ calculator/src/App.js
@@ -0,0 +1,11 @@
+import CalculatorFunc from "./Components/Calculator";
+
+function App() {
+  return (
+    <div className="App">
+      <CalculatorFunc />
+    </div>
+  );
+}
+
+export default App;
